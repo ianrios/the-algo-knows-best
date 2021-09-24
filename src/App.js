a9adf3884be982bd4d61a347114ad20b0622c0c3
Ian Rios <ianrios@Ians-MacBook-Pro.local>
2021-09-24 19:02:31 -0400

    added files for first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+
+import ExperimentPage from './Pages/ExperimentPage';
+import ResultsPage from './Pages/ResultsPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Navbar here
+
+      audio app here
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/pages/ExperimentPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ExperimentPage() {
+    return (
+        <div>
+            Experiment Page
+        </div>
+    )
+}
