bf914c4abdbcf8c165bc2a21d2de5aeaed6fa772
Ian Rios <ianrios@Ians-MacBook-Pro.local>
2021-10-06 00:04:23 -0400

    added create new user button

--- src/pages/ExperimentPage.js
+++ src/pages/ExperimentPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Spinner } from 'react-bootstrap'
 // import {
 //   Link,
 //   // useHistory
@@ -12,7 +12,7 @@ import QR from '../components/QR'
 import { Link } from 'react-router-dom'
 
 export default function ExperimentPage() {
-  const { updateUser, userData } = useAuth()
+  const { updateUser, userData, destroyStorage } = useAuth()
   const { mapUserDataToPlaylist, generateOrderedPlaylist, shufflePlaylist,
     // saveNewPlaylist
   } = usePlaylist()
@@ -25,6 +25,10 @@ export default function ExperimentPage() {
     updateUser()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const reset = () => {
+    destroyStorage()
+  }
 
   // useDeepCompareEffect(() => {
   //   // console.log({ shuffledPlaylist })
@@ -54,33 +58,32 @@ export default function ExperimentPage() {
         </Col>
       </Row>
       <Row>
-        <div className='col-lg-4 col-md-6 d-none d-md-block'>
+        <div className='col-lg-4 col-xl-3 offset-xl-1 col-md-6 d-none d-md-block text-center mx-auto'>
           <Row>
             <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-5 fw-bold'>
               The app requires listeners to click the save button in order to upload listening results to the database. This is to ensure that no "non-experiment" data is uploaded by accident.
             </Col>
           </Row>
-          <Row>
-            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-5'>
+          <Row className='border border-dark mt-5 px-3 mx-2 pt-3 pb-5 rounded-3 mx-auto'>
+            <div className='col-12 bg-light rounded-3 '>
               <h6>
                 If you have headphones, feel free to interact with the installation using your personal device.
               </h6>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="text-center mt-5">
+            </div>
+
+            <div className="col-12 mt-5 qr-bucket">
               <QR />
-            </Col>
+            </div>
           </Row>
           <Row>
-            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-5 text-center'>
+            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-3 text-center'>
               <h4>
                 Don't forget to press the save button!
               </h4>
             </Col>
           </Row>
         </div>
-        <div className='col-lg-4 col-md-6 col-sm-12' >
+        <div className='col-lg-4 col-xl-4 col-md-6 col-sm-12' >
           <AudioPlaylist
             playlist={shuffledPlaylist}
             setPlaylist={setShuffledPlaylist}
@@ -89,20 +92,27 @@ export default function ExperimentPage() {
             generating
           />
         </div>
-        <div className='col-lg-4 col-md-12 d-none d-md-block'>
+        <div className='col-lg-4 col-xl-3 offset-xl-1 col-md-12 d-none d-md-block mx-auto'>
           <Row>
-            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-5'>
+            <Col className='px-3 mx-2 py-2 mt-2 border border-dark rounded-3 '>
+              <h2>New Listener?</h2>
+              <button className="btn btn-primary mt-2" onClick={reset}>Click here to create a new user</button>
+              <p className='pt-3'>Current User ID: {!!userData.id ? userData.id : <Spinner animation='grow' size="sm" />}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-2'>
               Types of data recorded include: percent listened through, skip rate, number of times looped, likes and dislikes, song preference, playlist order preference, and when shuffled or ended.
             </Col>
           </Row>
           <Row>
-            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-5'>
+            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-3'>
               <p>This data is stored in a database and combined to generate a ranking system. Songs that have a high popularity appear higher in the <Link to='/results'>final result</Link>.</p>
               <p>All saved data that is recorded and will affect the resulting algorithmically generated playlist in real time.</p>
             </Col>
           </Row>
           <Row>
-            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-5'>
+            <Col className='bg-light rounded-3 px-3 mx-2 py-2 mt-3'>
               <p>You are currently listening to a traditional Fisher-Yates Shuffle Algorithm.</p>
               <p>Learn more by visiting the <Link to='/info'>info</Link> page.</p>
             </Col>
